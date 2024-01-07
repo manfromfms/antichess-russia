4863d6ea17e0f0c0abbaf65693f3039f6ee85837
normchell <cool.cool.alt1@gmail.com>
2024-01-07 20:51:35 +0500

    Добавлено автоматическое подтягивание списка игроков

---
+++ src/global/titles.js
@@ -0,0 +1,27 @@
+const titles = {
+    ASGM: [
+        "Bleak_Mind_Emptiness",
+        "Allegro_Moon",
+        "GrohochetGrom",
+    ],
+    AGM: [
+        "aleksschtin",
+        "asvy",
+        "skymocker",
+        "viAiv",
+    ],
+    AIM: [
+        "esko1779",
+        "VadimSt",
+    ],
+    AM: [
+        "bogdanyak",
+        "jannnn",
+        "antichesskiller",
+    ],
+    ACM: [
+        "glaue2dk",
+        "Screechy_123",
+        "Bullet_in_a_gun",
+    ],
+}
